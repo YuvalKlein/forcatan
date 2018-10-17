5a02f2dfb3d8761ccd198bf0bb1bb44a081aaaf2
Yuval <yuval@yk-group.org>
2018-10-17 09:26:46 +0300

    pause and reset

--- catanDice.js
+++ catanDice.js
@@ -1,5 +1,5 @@
 let selector = document.querySelector("#selector");
-let selectorButton = document.querySelector("#selectorButton");
+let selectorButton = document.querySelector("selectorButton");
 let info = document.querySelector("#info");
 let infoText = document.querySelector("#infoText");
 let dice = document.querySelectorAll(".dice");
@@ -7,32 +7,32 @@ let diceText1 = document.querySelector("#diceText1");
 let diceText2 = document.querySelector("#diceText2");
 let roll = document.querySelector("#roll");
 let timer = document.querySelector("#timer");
+let pause = document.querySelector("#pause");
 let reset = document.querySelector("#reset");
 let diceNum = document.querySelector("#diceNum");
 let times = document.querySelector("#times");
 let persentage = document.querySelector("#persentage");
 let tr = document.querySelector("#tr");
-let counterTime = selector.value;
+let turnTime = Number(selector.value);
 let diceResult = 0;
 let rolls = 0;
-
+let counterTime = 0;
+let startTimer;
 
 //COUNTER
-// selectorButton.addEventListener("click", function(){
-// 	countDown();
-// })
+selector.addEventListener("change", function(){
+	turnTime = Number(selector.value);
+})
 
-// function countDown(secs, elem) {
-// 	counterTime--;
-// 	counter = setTimeout(countDown(counterTime, timer),1000);
-// }
-
-// Create Table
-for(let i = 2; i <= 12; i++){
-	tr.append("<td id='diceNum'>" + i + "</td>" +
-	"<td id='times'>" +  + "</td>" + 
-	"<td id='persentage'>" +  + "</td>"
-	)
+function countDown() {
+	timer.innerHTML  = counterTime;
+	counterTime--;
+	if(counterTime > 0){
+		startTimer = setTimeout(countDown,1000);	
+	} else {
+		counterTime = turnTime;
+		startTimer = setTimeout(countDown,1000);
+	}
 }
 
 // ROLLING
@@ -41,10 +41,48 @@ roll.addEventListener("click", function(){
 	diceText2.textContent = rolling();
 	rolls += 1;
 	diceResult = Number(diceText1.textContent) + Number(diceText2.textContent);
-
+	if(counterTime > 0 ){
+		counterTime = turnTime;
+		clearTimeout(startTimer);
+		countDown();
+	} else {
+		counterTime = turnTime;
+		countDown();
+	}
 })
 
 const rolling = () => {
 	var random = Math.floor(Math.random() * 6 + 1);
 	return random;
+}
+
+// PAUSE
+let counting = 2;
+pause.addEventListener("click", function(){
+	console.log("1" + counting)
+	if(counting != 1){
+		counting = 1;
+		pause.innerHTML = "resume";
+		clearTimeout(startTimer);
+	} else {
+		counting = 2;
+		pause.innerHTML = "pause";
+		countDown()
+	}
+	console.log("2" + counting)
+
+});
+
+// RESET
+reset.addEventListener("click", function(){
+	counterTime = turnTime;
+	timer.innerHTML  = counterTime;
+});
+
+// Create Table
+for(let i = 2; i <= 12; i++){
+	tr.append("<td id='diceNum'>" + i + "</td>" +
+	"<td id='times'>" +  + "</td>" + 
+	"<td id='persentage'>" +  + "</td>"
+	)
 }
